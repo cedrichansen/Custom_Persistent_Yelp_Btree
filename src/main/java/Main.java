e8927d8083e785d27676fe6b2d665724e82f3648
cedrichansen <ceddy29@yahoo.ca>
2018-03-27 21:16:53 -0400

    testing adding stuff properly to tree

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,15 +12,27 @@ public class Main {
         try {
             String yelpFile = "../business.json";
             ReadJson rj = new ReadJson();
-           // ArrayList<YelpData> businesses = rj.readFromJsonSmall(yelpFile);
+            ArrayList<YelpData> businesses = new ArrayList<YelpData>();
             HashTable ht = rj.readToHash(yelpFile);
+
+            for (int i = 0; i<ht.table.length; i++) {
+                if (ht.table[i] != null) {
+                    for (int j = 0; j<ht.table[i].size(); j++) {
+                        businesses.add(ht.table[i].get(j));
+                    }
+                }
+            }
 
             System.out.println("done reading to the hashtable");
             System.out.println("current ht size is: " + ht.table.length);
             System.out.println();
 
             BTree bt = new BTree();
-            bt.insert(ht.table[3].get(0).hashCode());
+            //bt.insert(ht.table[3].get(0).hashCode());
+
+            for (int i = 0; i<10; i++) {
+                bt.insert(businesses.get(i).hashCode());
+            }
 
 
             System.out.println("done adding stuff to the btree");
